86040817f9eeedf56ece263ed280063e3bf59025
Gabriel Lopez <gabrielhenry.lopez@gmail.com>
2023-02-17 09:18:28 +0800

    Updates

--- src/assets/socials.js
+++
@@ -1,9 +0,0 @@
-
-const socials = {
-    linkedin: "https://www.linkedin.com/in/Gablooblue",
-    twitter: "https://twitter.com/TheGabLopez",
-    github: "https://github.com/Gablooblue",
-    email: "gabrielhenry.lopez@gmail.com"
-}
-
-export default socials
